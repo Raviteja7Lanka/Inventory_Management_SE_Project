87025a946f977e532be0d884cc62747b0e7c16bf
surya mandapati <mvsnraju19@gmail.com>
2022-03-04 20:36:51 -0500

    added products api and corrected an issue with Staff API

--- server/main.go
+++ server/main.go
@@ -17,6 +17,7 @@ func InitRouter() {
 	router.HandleFunc("/customer/all", getAllCustomer).Methods("GET")
 	router.HandleFunc("/supplier/all", getAllSupplier).Methods("GET")
 	router.HandleFunc("/staff/all", getAllStaff).Methods("GET")
+	router.HandleFunc("/product/all", getAllProduct).Methods("GET")
 	//
 
 	router.HandleFunc("/customer/order/{ordId}", getCustomerOrderByID).Methods("GET")
@@ -25,6 +26,7 @@ func InitRouter() {
 	router.HandleFunc("/customer/{cusId}", getCustomerByID).Methods("GET")
 	router.HandleFunc("/supplier/{supId}", getSupplierByID).Methods("GET")
 	router.HandleFunc("/staff/{staffId}", getStaffByID).Methods("GET")
+	router.HandleFunc("/product/{prodId}", getProductByID).Methods("GET")
 	//
 
 	router.HandleFunc("/customer/orders/add", addCustomerOrder).Methods("POST")
@@ -33,6 +35,7 @@ func InitRouter() {
 	router.HandleFunc("/customer/add", addCustomer).Methods("POST")
 	router.HandleFunc("/supplier/add", addSupplier).Methods("POST")
 	router.HandleFunc("/staff/add", addStaff).Methods("POST")
+	router.HandleFunc("/product/add", addProduct).Methods("POST")
 	//
 
 	router.HandleFunc("/customer/orders/{ordId}", updateCustomerOrder).Methods("PUT")
@@ -41,6 +44,7 @@ func InitRouter() {
 	router.HandleFunc("/customer/{cusId}", updateCustomer).Methods("PUT")
 	router.HandleFunc("/supplier/{supId}", updateSupplier).Methods("PUT")
 	router.HandleFunc("/staff/{staffId}", updateStaff).Methods("PUT")
+	router.HandleFunc("/product/{prodId}", updateProduct).Methods("PUT")
 	//
 
 	router.HandleFunc("/customer/orders/{ordId}", deleteCustomerOrder).Methods("DELETE")
@@ -49,6 +53,7 @@ func InitRouter() {
 	router.HandleFunc("/customer/{cusId}", deleteCustomer).Methods("DELETE")
 	router.HandleFunc("/supplier/{supId}", deleteSupplier).Methods("DELETE")
 	router.HandleFunc("/staff/{staffId}", deleteStaff).Methods("DELETE")
+	router.HandleFunc("/product/{prodId}", deleteProduct).Methods("DELETE")
 	//
 
 	http.Handle("/", router)
