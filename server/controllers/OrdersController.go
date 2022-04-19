78ee422d98a0aa65b659f133b4031acd57210084
Saikiran Madipadige <saikiranmadipadige@Saikirans-MacBook-Pro.local>
2022-04-19 00:28:56 -0400

    refactored changes

--- server/controllers/OrdersController.go
+++ server/controllers/OrdersController.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"apis/models"
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -11,81 +12,137 @@ import (
 
 var db *gorm.DB
 
-func GetAllOrders(w http.ResponseWriter, r *http.Request) {
-	// w.Header().Set("Content-Type", "application/json")
-	Orders := models.GetAllOrders()
-	res, e := json.Marshal(Orders)
-	if e != nil {
-		resp, _ := json.Marshal(map[string]string{"error": e.Error()})
-		http.Error(w, string(resp), http.StatusInternalServerError)
-	}
-	w.Header().Set("Content-Type", "pkglication/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
-}
+// func GetAllOrders(w http.ResponseWriter, r *http.Request) {
+// 	// w.Header().Set("Content-Type", "application/json")
+// 	Orders := models.GetAllOrders()
+// 	res, e := json.Marshal(Orders)
+// 	if e != nil {
+// 		resp, _ := json.Marshal(map[string]string{"error": e.Error()})
+// 		http.Error(w, string(resp), http.StatusInternalServerError)
+// 	}
+// 	w.Header().Set("Content-Type", "pkglication/json")
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Write(res)
+// }
 
-func GetOrderByID(w http.ResponseWriter, r *http.Request) {
-	// db.Raw("select id, date_of_order, order_details_id, Order_id, supplier_id, status from Orders where id=?",1).Scan(&order)
-	// data,_:= json.Marshal(&order)
-	// fmt.Fprint(w, string(data))
-	queryParams := mux.Vars(r)
-	// fmt.Println(queryParams["ordId"])
-	// db.First(&Orders, queryParams["ordId"])
-	Orders := models.GetOrderByID(queryParams["ordId"])
-	res, e := json.Marshal(Orders)
-	if e != nil {
-		http.Error(w, string(e.Error()), http.StatusInternalServerError)
-	}
-	w.Header().Set("Content-Type", "pkglication/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
-}
+// func GetOrderByID(w http.ResponseWriter, r *http.Request) {
+// 	// db.Raw("select id, date_of_order, order_details_id, Order_id, supplier_id, status from models.Orders where id=?",1).Scan(&order)
+// 	// data,_:= json.Marshal(&order)
+// 	// fmt.Fprint(w, string(data))
+// 	queryParams := mux.Vars(r)
+// 	// fmt.Println(queryParams["ordId"])
+// 	// db.First(&Orders, queryParams["ordId"])
+// 	Orders := models.GetOrderByID(queryParams["ordId"])
+// 	res, e := json.Marshal(Orders)
+// 	if e != nil {
+// 		http.Error(w, string(e.Error()), http.StatusInternalServerError)
+// 	}
+// 	w.Header().Set("Content-Type", "pkglication/json")
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Write(res)
+// }
 
-func AddOrder(w http.ResponseWriter, r *http.Request) {
+// func AddOrder(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	Orders := &models.Orders{}
+// 	json.NewDecoder(r.Body).Decode(&Orders)
+// 	newCust := models.AddOrder(Orders)
+// 	res, e := json.Marshal(newCust)
+// 	if e != nil {
+// 		http.Error(w, string(e.Error()), http.StatusInternalServerError)
+// 	}
+// 	w.Header().Set("Content-Type", "pkglication/json")
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Write(res)
+// 	// fmt.Println("Hello")
+// }
+
+// func UpdateOrder(w http.ResponseWriter, r *http.Request) {
+
+// 	updatedOrder := &models.Orders{}
+// 	queryParams := mux.Vars(r)
+// 	ordId := queryParams["ordId"]
+// 	json.NewDecoder(r.Body).Decode(&updatedOrder)
+// 	orders := models.UpdateOrder(ordId, updatedOrder)
+// 	res, e := json.Marshal(orders)
+// 	if e != nil {
+// 		resp, _ := json.Marshal(map[string]string{"error": e.Error()})
+// 		http.Error(w, string(resp), http.StatusInternalServerError)
+// 	}
+// 	w.Header().Set("Content-Type", "pkglication/json")
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Write(res)
+
+// }
+
+// func DeleteOrder(w http.ResponseWriter, r *http.Request) {
+
+// 	queryParams := mux.Vars(r)
+// 	Orders := models.DeleteOrder(queryParams["cusId"])
+// 	//db.Raw("delete from models.Orders where order_id=?", queryParams["ordId"])
+// 	res, e := json.Marshal(Orders)
+// 	if e != nil {
+// 		resp, _ := json.Marshal(map[string]string{"error": e.Error()})
+// 		http.Error(w, string(resp), http.StatusInternalServerError)
+// 	}
+// 	w.Header().Set("Content-Type", "pkglication/json")
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Write(res)
+
+// }
+
+func GetAllCustomerOrders(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	Orders := &models.Orders{}
-	json.NewDecoder(r.Body).Decode(&Orders)
-	newCust := models.AddOrder(Orders)
-	res, e := json.Marshal(newCust)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	var orders []models.Orders
+	e := db.Find(&orders).Error
 	if e != nil {
-		http.Error(w, string(e.Error()), http.StatusInternalServerError)
+		sendErr(w, http.StatusInternalServerError, e.Error())
+		return
 	}
-	w.Header().Set("Content-Type", "pkglication/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
+	e = json.NewEncoder(w).Encode(orders)
+	if e != nil {
+		sendErr(w, http.StatusInternalServerError, e.Error())
+	}
+
+}
+
+func GetCustomerOrderByID(w http.ResponseWriter, r *http.Request) {
+	var order models.Orders
+
+	queryParams := mux.Vars(r)
+	fmt.Println(queryParams["ordId"])
+	db.First(&order, queryParams["ordId"])
+	e := json.NewEncoder(w).Encode(order)
+	if e != nil {
+		sendErr(w, http.StatusInternalServerError, e.Error())
+	}
+}
+func AddCustomerOrder(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var order models.Orders
+	json.NewDecoder(r.Body).Decode(&order)
+	db.Create(&order)
+	json.NewEncoder(w).Encode(order)
 	// fmt.Println("Hello")
 }
-
-func UpdateOrder(w http.ResponseWriter, r *http.Request) {
-
-	updatedOrder := &models.Orders{}
+func UpdateCustomerOrder(w http.ResponseWriter, r *http.Request) {
+	var order models.Orders
+	var updatedOrder models.Orders
 	queryParams := mux.Vars(r)
 	ordId := queryParams["ordId"]
+	db.First(&order, ordId)
 	json.NewDecoder(r.Body).Decode(&updatedOrder)
-	orders := models.UpdateOrder(ordId, updatedOrder)
-	res, e := json.Marshal(orders)
-	if e != nil {
-		resp, _ := json.Marshal(map[string]string{"error": e.Error()})
-		http.Error(w, string(resp), http.StatusInternalServerError)
-	}
-	w.Header().Set("Content-Type", "pkglication/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
+	db.Model(&order).Where("order_id=?", ordId).Updates(&updatedOrder)
+	json.NewEncoder(w).Encode(&order)
 
 }
 
-func DeleteOrder(w http.ResponseWriter, r *http.Request) {
-
+func DeleteCustomerOrder(w http.ResponseWriter, r *http.Request) {
+	var order models.Orders
 	queryParams := mux.Vars(r)
-	Orders := models.DeleteOrder(queryParams["cusId"])
-	//db.Raw("delete from Orders where order_id=?", queryParams["ordId"])
-	res, e := json.Marshal(Orders)
-	if e != nil {
-		resp, _ := json.Marshal(map[string]string{"error": e.Error()})
-		http.Error(w, string(resp), http.StatusInternalServerError)
-	}
-	w.Header().Set("Content-Type", "pkglication/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
-
+	fmt.Println(queryParams["ordId"])
+	db.Delete(&order, queryParams["ordId"])
+	json.NewEncoder(w).Encode("{Status:200, Message: Deletion successful}")
 }
--- server/controllers/PaymentsController.go
+++ server/controllers/PaymentsController.go
@@ -1,90 +1,90 @@
 package controllers
 
-import (
-	"apis/models"
-	"encoding/json"
-	"net/http"
+// import (
+// 	"apis/models"
+// 	"encoding/json"
+// 	"net/http"
 
-	"github.com/gorilla/mux"
-)
+// 	"github.com/gorilla/mux"
+// )
 
 //var db *gorm.DB
 
-func GetAllPayments(w http.ResponseWriter, r *http.Request) {
-	// w.Header().Set("Content-Type", "application/json")
-	Payments := models.GetAllPayments()
-	res, e := json.Marshal(Payments)
-	if e != nil {
-		resp, _ := json.Marshal(map[string]string{"error": e.Error()})
-		http.Error(w, string(resp), http.StatusInternalServerError)
-	}
-	w.Header().Set("Content-Type", "pkglication/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
-}
+// func GetAllPayments(w http.ResponseWriter, r *http.Request) {
+// 	// w.Header().Set("Content-Type", "application/json")
+// 	Payments := models.GetAllPayments()
+// 	res, e := json.Marshal(Payments)
+// 	if e != nil {
+// 		resp, _ := json.Marshal(map[string]string{"error": e.Error()})
+// 		http.Error(w, string(resp), http.StatusInternalServerError)
+// 	}
+// 	w.Header().Set("Content-Type", "pkglication/json")
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Write(res)
+// }
 
-func GetPaymentByID(w http.ResponseWriter, r *http.Request) {
-	// db.Raw("select id, date_of_order, order_details_id, Order_id, supplier_id, status from Orders where id=?",1).Scan(&order)
-	// data,_:= json.Marshal(&order)
-	// fmt.Fprint(w, string(data))
-	queryParams := mux.Vars(r)
-	// fmt.Println(queryParams["ordId"])
-	// db.First(&Orders, queryParams["ordId"])
-	Orders := models.GetPaymentByID(queryParams["payId"])
-	res, e := json.Marshal(Orders)
-	if e != nil {
-		http.Error(w, string(e.Error()), http.StatusInternalServerError)
-	}
-	w.Header().Set("Content-Type", "pkglication/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
-}
+// func GetPaymentByID(w http.ResponseWriter, r *http.Request) {
+// 	// db.Raw("select id, date_of_order, order_details_id, Order_id, supplier_id, status from Orders where id=?",1).Scan(&order)
+// 	// data,_:= json.Marshal(&order)
+// 	// fmt.Fprint(w, string(data))
+// 	queryParams := mux.Vars(r)
+// 	// fmt.Println(queryParams["ordId"])
+// 	// db.First(&Orders, queryParams["ordId"])
+// 	Orders := models.GetPaymentByID(queryParams["payId"])
+// 	res, e := json.Marshal(Orders)
+// 	if e != nil {
+// 		http.Error(w, string(e.Error()), http.StatusInternalServerError)
+// 	}
+// 	w.Header().Set("Content-Type", "pkglication/json")
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Write(res)
+// }
 
-func AddPayment(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	Payments := &models.Payments{}
-	json.NewDecoder(r.Body).Decode(&Payments)
-	newCust := models.AddPayment(Payments)
-	res, e := json.Marshal(newCust)
-	if e != nil {
-		http.Error(w, string(e.Error()), http.StatusInternalServerError)
-	}
-	w.Header().Set("Content-Type", "pkglication/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
-	// fmt.Println("Hello")
-}
+// func AddPayment(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	Payments := &models.Payments{}
+// 	json.NewDecoder(r.Body).Decode(&Payments)
+// 	newCust := models.AddPayment(Payments)
+// 	res, e := json.Marshal(newCust)
+// 	if e != nil {
+// 		http.Error(w, string(e.Error()), http.StatusInternalServerError)
+// 	}
+// 	w.Header().Set("Content-Type", "pkglication/json")
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Write(res)
+// 	// fmt.Println("Hello")
+// }
 
-func UpdatePayment(w http.ResponseWriter, r *http.Request) {
+// func UpdatePayment(w http.ResponseWriter, r *http.Request) {
 
-	updatedPayment := &models.Payments{}
-	queryParams := mux.Vars(r)
-	payId := queryParams["payId"]
-	json.NewDecoder(r.Body).Decode(&updatedPayment)
-	payments := models.UpdatePayment(payId, updatedPayment)
-	res, e := json.Marshal(payments)
-	if e != nil {
-		resp, _ := json.Marshal(map[string]string{"error": e.Error()})
-		http.Error(w, string(resp), http.StatusInternalServerError)
-	}
-	w.Header().Set("Content-Type", "pkglication/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
+// 	updatedPayment := &models.Payments{}
+// 	queryParams := mux.Vars(r)
+// 	payId := queryParams["payId"]
+// 	json.NewDecoder(r.Body).Decode(&updatedPayment)
+// 	payments := models.UpdatePayment(payId, updatedPayment)
+// 	res, e := json.Marshal(payments)
+// 	if e != nil {
+// 		resp, _ := json.Marshal(map[string]string{"error": e.Error()})
+// 		http.Error(w, string(resp), http.StatusInternalServerError)
+// 	}
+// 	w.Header().Set("Content-Type", "pkglication/json")
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Write(res)
 
-}
+// }
 
-func DeletePayment(w http.ResponseWriter, r *http.Request) {
+// func DeletePayment(w http.ResponseWriter, r *http.Request) {
 
-	queryParams := mux.Vars(r)
-	Payments := models.DeletePayment(queryParams["payId"])
-	//db.Raw("delete from Orders where order_id=?", queryParams["ordId"])
-	res, e := json.Marshal(Payments)
-	if e != nil {
-		resp, _ := json.Marshal(map[string]string{"error": e.Error()})
-		http.Error(w, string(resp), http.StatusInternalServerError)
-	}
-	w.Header().Set("Content-Type", "pkglication/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
+// 	queryParams := mux.Vars(r)
+// 	Payments := models.DeletePayment(queryParams["payId"])
+// 	//db.Raw("delete from Orders where order_id=?", queryParams["ordId"])
+// 	res, e := json.Marshal(Payments)
+// 	if e != nil {
+// 		resp, _ := json.Marshal(map[string]string{"error": e.Error()})
+// 		http.Error(w, string(resp), http.StatusInternalServerError)
+// 	}
+// 	w.Header().Set("Content-Type", "pkglication/json")
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Write(res)
 
-}
+// }
